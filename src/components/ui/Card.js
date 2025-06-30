f0737a9e2ebd84e746c7b110f7e2cc7148c45ec1
seakh-masum <masum0aktar@gmail.com>
2025-06-30 15:34:25 +0530

    develop print functionality & enhance design to v2.0

--- src/components/ui/Card.js
+++ src/components/ui/Card.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React from "react";
 
 export const Card = (props) => {
-	return (
-		<div className='bg-white rounded-2xl shadow-lg p-4 relative text-black/90 block mb-4 dark:bg-neutral-900 dark:text-white'>
-			{props.header && <h1 className='text-2xl font-montserrat font-semibold pt-0 pb-4 px-2 dark:text-white'>{props.header}</h1>}
-			{props.children}
-		</div>
-	);
+  return (
+    <div className="bg-neutral-50 rounded-2xl shadow-lg p-4 relative text-black/90 block mb-4 dark:bg-neutral-900 dark:text-white">
+      {props.header && (
+        <h1 className="text-2xl font-montserrat font-semibold pt-0 pb-4 px-2 dark:text-white">
+          {props.header}
+        </h1>
+      )}
+      {props.children}
+    </div>
+  );
 };
