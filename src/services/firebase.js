8eee2d092fd871529d68e408eb7eb065703e926f
seakh-masum <masum0aktar@gmail.com>
2025-09-05 01:28:40 +0530

    added env props

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA10_Hne0PlOMyjpl3AMXVZWPczLyBJHaE",
-  authDomain: "masum-cv.firebaseapp.com",
-  databaseURL: "https://masum-cv.firebaseio.com",
-  projectId: "masum-cv",
-  storageBucket: "masum-cv.appspot.com",
-  messagingSenderId: "733514146710",
-  appId: "1:733514146710:web:556a863030a39819b673b7",
-  measurementId: "G-1WQGXG27F3",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 // Initialize Firebase and Firestore
